9e9c6068ec2474013ac28133414aff4d43b470d0
Eduardo Kraus <kraus@eduardokraus.com>
2026-02-07 11:45:51 -0300

    moodle.Files.MoodleInternal.MoodleInternalGlobalState

--- mod_form.php
+++ mod_form.php
@@ -22,6 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+// phpcs:disable moodle.Files.MoodleInternal.MoodleInternalGlobalState
 require_once($CFG->dirroot . "/course/moodleform_mod.php");
 
 /**
