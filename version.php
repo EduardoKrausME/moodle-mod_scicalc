17502612ab93007d2ece67af21fe4984a86f419f
Eduardo Kraus <kraus@eduardokraus.com>
2026-02-10 22:15:17 -0300

    V 1.0.6

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026020705;
-$plugin->release = "1.0.5";
+$plugin->version = 2026021000;
+$plugin->release = "1.0.6";
 $plugin->component = "mod_scicalc";
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
